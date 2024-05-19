76e49bc87d44bdda8cf6e9213e9803c897113aa3
Kanishk Pandey <kanishkkpandey@gmail.com>
2024-05-19 13:14:36 +0530

    Add CORE_CONCEPTS using props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
+import ReactLogo from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data";
+
+const adjectives = ["Fundamental", "Core", "Essential", "Foundational"];
+const random_adjective =
+  adjectives[Math.floor(Math.random() * adjectives.length)];
+
 function Header() {
   //This is a component in react, function must start with an uppercase letter and must return renderable content
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={ReactLogo} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {random_adjective} React concepts you will need for almost any app you
+        are going to build!
       </p>
     </header>
   );
 }
 
+function CoreConcepts({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
 function App() {
   return (
     <div>
@@ -19,6 +35,15 @@ function App() {
       <Header></Header>
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts {...CORE_CONCEPTS[0]} />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
