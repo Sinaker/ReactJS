e263b7f1a193f28cdb84d3eeadd67728fcf87a42
Kanishk Pandey <kanishkkpandey@gmail.com>
2024-05-20 10:24:16 +0530

    Finish Project-1

--- src/components/Header/TabButton.jsx
+++ src/components/Header/TabButton.jsx
@@ -1,11 +1,8 @@
-function clickHandler() {
-  console.log("Button Clicked!");
-}
-
-export default function TabButton({ children, onSelect, isSelected }) {
+// Using Proxy Props to forward attributes from custom Tags to actual taf
+export default function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? "active" : ""} onClick={onSelect}>
+      <button className={isSelected ? "active" : ""} {...props}>
         {children}
       </button>
     </li>
