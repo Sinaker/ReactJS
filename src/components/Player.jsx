21e46e4ac043fe2e531b9cb828e2a7789c1ad41d
Kanishk Pandey <135948593+Sinaker@users.noreply.github.com>
2024-06-02 21:37:41 +0530

    Implement Popup without Display Logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,30 @@
+import { useState, useRef } from "react"; //Using refs
+
 export default function Player() {
+  const inputRef = useRef();
+  const [playerName, setPlayerName] = useState(null);
+
+  function handleClick() {
+    //Let's use the inputRef to access the input component
+    //Now I can access all of the attributes of the connected component
+    setPlayerName(inputRef.current.value); //inputRef always has an attribute of 'current'
+    inputRef.current.value = "";
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName ? playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          ref={inputRef} //ref is a default arg in all react components
+          onKeyUp={(event) => {
+            if (event.key === "Enter") {
+              handleClick();
+            }
+          }}
+        />
+        <button onClick={handleClick}>Set Name</button>
+        {/* Now we can get name in button click instead of every keystroke*/}
       </p>
     </section>
   );
---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,49 @@
+import { useState, useRef } from "react";
+import ResultsWindow from "./ResultsWindow.jsx";
+
+export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
+  const dialog = useRef();
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+  let timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open(); //You lost
+    setTimeRemaining(targetTime * 1000); //If I don't put this timeRemaining will always be <=0 executing an inf loop (cuz of setState)
+  }
+
+  function startTimer() {
+    timer.current = setInterval(() => {
+      //setInterval performs the operation every 'x' milliseconds
+      setTimeRemaining((prevTime) => prevTime - 10);
+    }, 10);
+  }
+
+  function stopTimer() {
+    dialog.current.open(); //Manually pressed stop button
+    clearInterval(timer.current);
+    setTimeRemaining(targetTime * 1000); //Reset target time
+  }
+
+  return (
+    <>
+      <ResultsWindow ref={dialog} targetTime={targetTime} result="lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timeRemaining <= 0 ? <p>You lost</p> : ""}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerIsActive ? stopTimer : startTimer}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
+          </button>
+        </p>
+        <p className={timerIsActive ? "active" : undefined}>
+          {timerIsActive ? "Timer is running" : "Timer Stopped"}
+        </p>
+      </section>
+    </>
+  );
+}
